be148ecb2cca372a98199f019bc6b7665004b749
beaumanvienna (JC) <beaumanvienna@gmail.com>
2021-09-27 19:05:38 -0700

    GitHub workflow for Windows added

---
+++ vendor/premake/premake5.lua
@@ -0,0 +1,29 @@
+
+function file_exists(filename)
+	local filehandle=io.open(filename,"r")
+	if filehandle~=nil then
+		io.close(filehandle)
+		return true
+	else
+		return false
+	end
+end
+
+project "Premake"
+	kind "Utility"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{wks.location}/**premake5.lua"
+	}
+
+	if file_exists("bin/premake5.exe") then
+		postbuildmessage "Regenerating project files with Premake5!"
+		postbuildcommands
+		{
+			"\"%{prj.location}bin/premake5\" %{_ACTION} --file=\"%{wks.location}premake5.lua\""
+		}
+	end
